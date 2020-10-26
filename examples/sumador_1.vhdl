9dfa10abb2f552d0977c7e8dab8bd44746be47bc
Joaquin Arias <joaquin.arias@software.imdea.org>
2020-10-26 11:45:12 +0100

    Estructura archivo

---
+++ examples/sumador_1.vhdl
@@ -0,0 +1,12 @@
+-- example from: https://jnjsite.com/vhdl-sumador-completo-de-1-bit/
+-- Sumador completo de 1 bit
+
+-- Librerías
+
+
+
+-- Declaración de la entidad del circuito
+
+
+
+-- Arquitectura del circuito
