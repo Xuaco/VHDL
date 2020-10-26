0944f02abf14b276f26ccd40aecc12d6572c1e03
Joaquin Arias <joaquin.arias@software.imdea.org>
2020-10-26 11:47:13 +0100

    Arquitectura completa del circuito.

--- examples/sumador_1.vhdl
+++ examples/sumador_1.vhdl
@@ -17,3 +17,33 @@ entity oneBitFullAdder is
 end entity;
 
 -- Arquitectura del circuito
+
+architecture arch of oneBitFullAdder is
+
+    signal auxInVect : std_logic_vector (2 downto 0);
+
+begin
+
+    auxInVect(2) <= carryIn;
+    auxInVect(1) <= a;
+    auxInVect(0) <= b;
+
+    sum <= '0' when auxInVect = "000" else
+        '1' when auxInVect = "001" else
+        '1' when auxInVect = "010" else
+        '0' when auxInVect = "011" else
+        '1' when auxInVect = "100" else
+        '0' when auxInVect = "101" else
+        '0' when auxInVect = "110" else
+        '1';
+
+    carryOut <= '0' when auxInVect = "000" else
+        '0' when auxInVect = "001" else
+        '0' when auxInVect = "010" else
+        '1' when auxInVect = "011" else
+        '0' when auxInVect = "100" else
+        '1' when auxInVect = "101" else
+        '1' when auxInVect = "110" else
+        '1';
+
+end architecture;
